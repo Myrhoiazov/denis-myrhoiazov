31a5c1f2a3ce0b9b19514db44e5525c67867d0c7
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-10-05 21:55:55 +0300

    rename url

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const AboutFilmsPage = lazy(() => import('./pages/AboutFilms'));
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/my-portfolio">
+    <BrowserRouter basename="/denis-myrhoiazov">
       <Suspense fallback={<Loader />}>
       <Routes>
         {/* <Route path="/" element={<Header />}> */}

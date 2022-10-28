e7868612301e808ae2495435040c9c6d1ba4d249
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-10-28 18:04:51 +0200

    add contact footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,39 @@
 import Container from 'components/container';
+import { useMediaQuery } from 'react-responsive';
 import s from './Footer.module.scss';
 
 const Footer = () => {
+  const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
+
   return (
-    <div className={s.container}>
-      <Container>
-        <div className={s.content}>
-          <p className={s.text}> Content</p>
+    <>
+      {isDesktop && (
+        <div className={s.container}>
+          <Container>
+            <div className={s.content}>
+              <p className={s.text}> My resume application</p>
+              <ul className={s.items}>
+                <li className={s.item}>
+                  <a href='https://github.com/Myrhoiazov' target='blank' className={s.link}>Github</a>
+                </li>
+                <li className={s.item}>
+                  <a href='https://github.com/Myrhoiazov' target='blank' className={s.link}>Telegram</a>
+                </li>
+                <li className={s.item}>
+                  <a href='https://github.com/Myrhoiazov' target='blank' className={s.link}>Facebook</a>
+                </li>
+                <li className={s.item}>
+                  <a href="mailto:denis.mirgoyazov@gmail.com" className={s.link}>denis.mirgoyazov@gmail.com</a>
+                </li>
+                <li className={s.item}>
+                  <a href="tel:+80985245200" className={s.link}>+38 (098) 524-52-00</a>
+                </li>
+              </ul>
+            </div>
+          </Container>
         </div>
-      </Container>
-    </div>
+      )}
+    </>
   );
 };
 

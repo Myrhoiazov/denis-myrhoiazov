dc3f8669a7a7e643660e9d358f1f553a2234d7d1
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-10-31 20:02:14 +0100

    add skills

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,12 +1,18 @@
 import Container from 'components/container';
 import s from './Skills.module.scss';
+import { skillList } from './skillList';
 
 const Skills = () => {
   return (
     <>
       <Container>
         <div className={s.inner_wrapper}>
-          <h2>Skills</h2>
+          <h2 className={s.title}>Skills</h2>
+          <ul className={s.items}>
+            {skillList.map(({ skills}) => (
+              <li key={skills} className={s.item}>{skills}</li>
+            ))}
+          </ul>
         </div>
       </Container>
     </>
---
+++ src/pages/Skills/skillList.js
@@ -0,0 +1,39 @@
+export const skillList = [
+  {
+    skills: 'HTML5',
+    status: 'done',
+    icon: '',
+  },
+  {
+    skills: 'CSS/SASS',
+    status: 'done',
+    icon: '',
+  },
+  {
+    skills: 'JS',
+    status: 'done',
+    icon: '',
+  },
+  {
+    skills: 'React.js',
+    status: 'done',
+    icon: '',
+  },
+  {
+    skills: 'Node.js',
+    status: 'done',
+    icon: 'in process',
+  },
+  {
+    skills: 'Type Script',
+    status: 'done',
+    icon: 'in process',
+  },
+  {
+    skills: 'React Native',
+    status: 'done',
+    icon: 'in process',
+  },
+];
+
+export default skillList;
